58fba39bddb15847d4b842584b60c38bcff446e3
Kirill Nemtyrev <75456383+KirillNemtyrev@users.noreply.github.com>
2021-08-17 00:06:06 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1382,4 +1382,4 @@ if __name__ == "__main__":
         except Exception as e:
             continue
 
-    executor.start_polling(dp, skip_updates=False)
+    executor.start_polling(dp, skip_updates=True)
